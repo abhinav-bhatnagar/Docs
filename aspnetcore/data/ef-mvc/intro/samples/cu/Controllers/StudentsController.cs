35d65754c0d523481d85a8714d799c3aa0116222
Tom Dykstra <tdykstra@microsoft.com>
2017-04-11 16:41:12 -0700

    expression trees

--- aspnetcore/data/ef-mvc/intro/samples/cu/Controllers/StudentsController.cs
+++ aspnetcore/data/ef-mvc/intro/samples/cu/Controllers/StudentsController.cs
@@ -1,4 +1,4 @@
-#define SortFilterPage //or ScaffoldedIndex or SortOnly or SortFilter
+#define SortFilterPage //or ScaffoldedIndex or SortOnly or SortFilter or DynamicLinq
 #define ReadFirst //or CreateAndAttach
 #define DeleteWithReadFirst // or DeleteWithoutReadFirst
 
@@ -11,6 +11,8 @@ using ContosoUniversity.Data;
 using ContosoUniversity.Models;
 using System;
 using Microsoft.Extensions.Logging;
+using System.Linq.Expressions;
+using System.Reflection;
 
 #region snippet_Context
 namespace ContosoUniversity.Controllers
@@ -141,6 +143,84 @@ namespace ContosoUniversity.Controllers
 
             int pageSize = 3;
             return View(await PaginatedList<Student>.CreateAsync(students.AsNoTracking(), page ?? 1, pageSize));
+        }
+        #endregion  
+#elif (DynamicLinq)
+        #region snippet_DynamicLinq
+        public async Task<IActionResult> Index(
+            string sortOrder,
+            string currentFilter,
+            string searchString,
+            int? page)
+        {
+            ViewData["CurrentSort"] = sortOrder;
+            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "LastName_desc" : "";
+            ViewData["DateSortParm"] = sortOrder == "EnrollmentDate" ? "EnrollmentDate_desc" : "EnrollmentDate";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewData["CurrentFilter"] = searchString;
+
+            var students = from s in _context.Students
+                           select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                students = students.Where(s => s.LastName.Contains(searchString)
+                                       || s.FirstMidName.Contains(searchString));
+            }
+
+            if (string.IsNullOrEmpty(sortOrder))
+            {
+                sortOrder = "LastName";
+            }
+
+            bool descending = false;
+            if (sortOrder.EndsWith("_desc"))
+            {
+                sortOrder = sortOrder.Substring(0, sortOrder.Length - 5);
+                descending = true;
+            }
+
+            students = SortCollectionBy(students, String.IsNullOrEmpty(sortOrder) ? "LastName" : sortOrder, descending);
+
+            int pageSize = 3;
+            return View(await PaginatedList<Student>.CreateAsync(students.AsNoTracking(), page ?? 1, pageSize));
+        }
+
+        private static IQueryable<T> SortCollectionBy<T>(IQueryable<T> collection, string propertyName, bool descending)
+        {
+            // Build the parameter expression for an item in the collection.
+            // this would be the "p => " in a "p => p.LastName" orderby expression.
+            var parmExpression = Expression.Parameter(typeof(T), "item");
+
+            // Build the property access expression. This would be the "p.LastName"
+            // part of the "p => p.LastName" expression.
+            var lambdaBody = Expression.Property(parmExpression, propertyName);
+
+            // Get the type of the named property. 
+            // Note that this will throw ArgumentException for a property that is not found.
+            var propertyType = typeof(T).GetProperty(propertyName).PropertyType;
+
+            // Build an OrderBy:
+            MethodCallExpression orderByCall = Expression.Call(
+                    typeof(Queryable),
+                    descending ? "OrderByDescending" : "OrderBy",
+                    new Type[] { typeof(T), propertyType },
+                    collection.Expression,
+                    Expression.Lambda(lambdaBody, parmExpression));
+
+            // Create the query method call:
+            var sortedIQueryable = collection.Provider.CreateQuery<T>(orderByCall);
+
+            return sortedIQueryable;
+
         }
         #endregion  
 #endif
